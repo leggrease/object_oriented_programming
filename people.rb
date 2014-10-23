caec79e70a8cab2885fd54085c7fed69cf63deef
Manny Berkal-Sarbit <mannyberkalsarbit@gmail.com>
2014-10-23 14:22:15 -0400

    first excercise

---
+++ people.rb
@@ -0,0 +1,37 @@
+class Person
+	attr_accessor :name
+	def initialize(name)
+		@name = name
+	end 
+
+	def greeting
+		puts "Hi my name is #{@name}"
+	end
+end
+
+class Student < Person
+	def learn
+		puts "I get it!"
+	end
+end
+
+class Instructor < Person
+	def teach
+		puts "Everything in Ruby is an Object!"
+	end
+end
+
+Chris = Instructor.new("Chris")
+
+Christina = Student.new("Christina")
+
+Chris.greeting
+
+Christina.greeting
+
+Chris.teach
+
+Christina.learn
+
+#It doesn't work because the Instructor class is not a parent or child class of 
+#student so it does not inherit the teach method
